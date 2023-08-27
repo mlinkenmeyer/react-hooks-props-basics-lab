f02c3e71a2f09e41516d9a849ebb5aa7d5e6ec94
Madison <mlinkenmeyer@gmail.com>
2023-08-26 20:21:08 -0700

    props-lab-completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ user }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio ? <p>{user.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links
+        user={user}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +10,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home user={user} />
+      <About user={user} />
     </div>
   );
 }
